6ec312f90311c35b7cc214f73eeedae6153a6049
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2023-07-22 10:59:07 +0530

    updated server

--- server/model/shortStore.js
+++ server/model/shortStore.js
@@ -1,16 +1,21 @@
-import mongoose from 'mongoose';
-import shortId from 'shortid';
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
 
 const shortSchema = new mongoose.Schema({
-    full: {
-      type: String,
-      required: true
-    },
-    short: {
-      type: String,
-      required: true,
-      default: shortId.generate
-    }
-  });
-  
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+    default: () => nanoid().substring(0, 10),
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600,
+  },
+});
+
 export default mongoose.model("ShortUrl", shortSchema);
--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import shortUrl from './model/shortStore.js';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import shortUrl from "./model/shortStore.js";
+import cors from "cors";
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/shortUrlDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://127.0.0.1:27017/shortUrlDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //use cors to allow cross origin resource sharing
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
@@ -18,33 +21,29 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello There!');
+app.get("/", (req, res) => {
+  res.send("Hello There!");
 });
 
-app.post('/short', async (req, res) => {
-  const found = await shortUrl.find({full: req.body.full});
-  if(found.length>0){
+app.post("/short", async (req, res) => {
+  const found = await shortUrl.find({ full: req.body.full });
+  if (found.length > 0) {
     res.send(found);
-  }
-  else{
-    await shortUrl.create({full: req.body.full});
-    const foundNow = await shortUrl.find({full: req.body.full});
+  } else {
+    await shortUrl.create({ full: req.body.full });
+    const foundNow = await shortUrl.find({ full: req.body.full });
     res.send(foundNow);
   }
 });
 
-app.get('/:shortUrl', async (req, res) => {
+app.get("/:shortUrl", async (req, res) => {
   const short = await shortUrl.findOne({ short: req.params.shortUrl });
   if (short == null) return res.sendStatus(404);
   res.redirect(`${short.full}`);
 });
 
-let port = process.env.PORT;
-if (port == null || port == ""){
-  port = 5000;
-}
+let port = process.env.PORT || 5000;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Server started successfully on port: ", port);
 });
